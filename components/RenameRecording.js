75da9097abf91467904cce1c197825399fbbc29e
Luyanda <luyandashamase5@gmail.com>
2025-01-23 10:35:53 +0200

    Feat: RenameRecording component coded and updated

---
+++ components/RenameRecording.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button, TextInput, View, StyleSheet } from 'react-native';
+
+const RenameRecording = ({ newName, onChangeName, onSave }) => (
+  <View style={styles.renameContainer}>
+    <TextInput
+      style={styles.renameInput}
+      value={newName}
+      onChangeText={onChangeName}
+      placeholder="Enter new name"
+    />
+    <Button title="Save" onPress={onSave} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  renameContainer: {
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  renameInput: {
+    borderWidth: 1,
+    padding: 8,
+    marginBottom: 10,
+    borderColor: '#ccc',
+  },
+});
+
+export default RenameRecording;
